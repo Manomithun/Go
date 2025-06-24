424d7020511e3358ce5027f6870e399208d5a07e
Manomithun <manomarutharasu@gmail.com>
2025-06-24 09:58:08 +0530

    gorutine Channel program added

---
+++ Concurrent.go
@@ -0,0 +1,16 @@
+package main 
+import "fmt"
+func main(){
+	ch:=make(chan int)
+	go call(ch)
+	for a:=range ch{
+		fmt.Println(a);;
+	}
+	
+}
+func call(out chan int){
+	for i:=0;i<5;i++{
+		out<-i
+	}
+	close(out);
+}
